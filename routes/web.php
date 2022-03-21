698a392a351d762d3b049337e3e243ea0c59a49b
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2022-03-21 14:14:01 +0700

    membersihkan bagian comment

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\PageController::class, 'index'])->name('home');
-
 Route::get('/', function () {
     return view('index');
 });
