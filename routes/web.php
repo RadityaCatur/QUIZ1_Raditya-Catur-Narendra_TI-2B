bfdbdd24808913e16baa0c8e8adfdb81d470bd8f
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2022-03-21 14:12:49 +0700

    Menambahkan controller dan mengganti route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\PageController::class, 'index'])->name('home');
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
